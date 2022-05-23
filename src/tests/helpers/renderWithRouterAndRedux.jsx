f573df93c19a9297e67a4309637696ccc062b4c1
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-05-23 11:07:41 -0300

    feat(tests): create tests folder, config lint  and render with router redux helper

---
+++ src/tests/helpers/renderWithRouterAndRedux.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../../store/reducers';
+
+const renderWithRouterAndRedux = (
+  component,
+  {
+    initialState = {},
+
+    store = createStore(rootReducer, initialState),
+
+    initialEntries = ['/'],
+
+    history = createMemoryHistory({ initialEntries }),
+  } = {},
+) => ({
+  ...render(
+    <Router history={history}>
+      <Provider store={store}>
+        {component}
+      </Provider>
+    </Router>,
+  ),
+  history,
+  store,
+});
+
+export default renderWithRouterAndRedux;
