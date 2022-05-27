21bde292fa3da39cbb45404231b27669593a9f43
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-05-27 01:31:08 -0300

    feat: add styled components reset css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: normal;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`;
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Login} />
-      <Route exact path="/carteira" component={Wallet} />
-    </Switch>
+    <div>
+      <GlobalStyle />
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/carteira" component={Wallet} />
+      </Switch>
+    </div>
   );
 }
 
