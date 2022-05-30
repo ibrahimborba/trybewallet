b427dbca9aa34e9cf9906fbeb9a69d62f885c80f
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-05-30 03:31:42 -0300

    change wallet route

--- src/App.js
+++ src/App.js
@@ -79,10 +79,10 @@ class App extends React.Component {
         <ThemeProvider theme={palette}>
           <GlobalStyle palette={palette} />
           <Switch>
-            <Route exact path="https://ibrahimborba.github.io/trybewallet/" component={Login} />
+            <Route exact path="/" component={Login} />
             <Route
               exact
-              path="https://ibrahimborba.github.io/trybewallet/carteira"
+              path="https://ibrahimborba.github.io/trybewallet/"
               render={(props) => (
                 <Wallet
                   {...props}
